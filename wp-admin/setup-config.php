026cb3cef0c709e44eacfe7de1151ebf7736317c
sato-eriko <sato-eriko@9105-109620.local>
2018-05-08 11:34:29 +0900

    テストコメント

--- wp-admin/setup-config.php
+++ wp-admin/setup-config.php
@@ -191,7 +191,7 @@ switch($step) {
 	?>
 <h1 class="screen-reader-text"><?php _e( 'Set up your database connection' ) ?></h1>
 <form method="post" action="setup-config.php?step=2">
-	<p><?php _e( 'Below you should enter your database connection details. If you&#8217;re not sure about these, contact your host.' ); ?></p>
+	<p><?php _e( 'Below you should enter your database connection details. If you&#8217;re not sure about these, contact your host.これはテスト' ); ?></p>
 	<table class="form-table">
 		<tr>
 			<th scope="row"><label for="dbname"><?php _e( 'Database Name' ); ?></label></th>
